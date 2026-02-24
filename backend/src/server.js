df31534b3c9e02981a2a9c057b055a3e8786b1fa
GJV PAVAN SAI <pavansai20052004@gmail.com>
2026-02-24 22:49:55 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,10 @@ import serviceRequestRoutes from "./routes/serviceRequestRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("HunarHub API running ✅"));
